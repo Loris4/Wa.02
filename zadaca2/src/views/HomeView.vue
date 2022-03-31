a5801fc4bf3c795016b669e7dc83e4f60bff266c
Loris Lukić <loris.lukic13@gmail.com>
2022-03-31 10:50:15 +0200

    Drugi commit

--- zadaca2/src/views/HomeView.vue
+++ zadaca2/src/views/HomeView.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="6" md="3"> </v-col>
+      <v-form>
+        <v-text-field label="Regular" v-model="name"></v-text-field>
+        <v-btn @click="getData"> Submit </v-btn>
+        <v-data-table
+          :headers="headers"
+          :items="items"
+          :items-per-page="9"
+          class="elevation-1"
+        ></v-data-table>
+      </v-form>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
